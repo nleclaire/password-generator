2a226744737416d5c56e9adb01679ac875afb890
Noah LeClaire <noahleclaire@gmail.com>
2021-06-15 18:33:33 -0400

    Adding header - heart icon, font family, janky padding fix for title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './component/button/button.component';
+import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
